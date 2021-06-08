86d5d00e5ba8f079c8bb28bb14fa6b9cd56d8570
JCDMeira <jean.meira10@hotmail.com>
2021-06-08 17:49:27 -0300

    Adiciona controllers

---
+++ src/controllers/SettingsController.ts
@@ -0,0 +1,24 @@
+import { Request, Response} from "express";
+import { getCustomRepository } from "typeorm";
+
+// # Arquivos próprios
+import { SettingsRepository } from "../repositories/SettingsRepository";
+
+class SettingsController{
+
+    async create(request: Request, response: Response){
+        const { chat, username} = request.body;
+    const settingsRepository = getCustomRepository(SettingsRepository);
+
+    const settings = settingsRepository.create({
+        chat,
+        username
+    })
+
+    await settingsRepository.save(settings);
+
+    return response.json(settings);
+    }
+}
+
+export { SettingsController }
