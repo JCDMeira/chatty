42b453422d16afe6ccacfab649ad99f951cb9d14
JCDMeira <jean.meira10@hotmail.com>
2021-06-11 16:28:01 -0300

    Adiciona renderização de mensagem para admin

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,7 +1,8 @@
 const socket = io();
+let connectionsUsers = [];
 
 socket.on("admin_list_all_users", (connections) => {
-    console.log(connections);
+    connectionsUsers = connections;
     document.getElementById("list_users").innerHTML = "";
 
     let template = document.getElementById("template").innerHTML;
@@ -15,3 +16,44 @@ socket.on("admin_list_all_users", (connections) => {
         document.getElementById("list_users").innerHTML += rendered;
     });
 });
+
+function call(id) {
+    const connection = connectionsUsers.find(connection => connection.socket_id === id);
+
+    const template = document.getElementById("admin_template").innerHTML;
+
+    const rendered = Mustache.render(template, {
+        email: connection.user.email,
+        id: connection.user_id,
+    });
+
+    document.getElementById("supports").innerHTML += rendered;
+
+    const params = {
+        user_id: connection.user_id
+    };
+
+    socket.emit("admin_list_messages_by_user", params, messages => {
+        const divMessages = document.getElementById(`allMessages${connection.user_id}`);
+
+        messages.forEach(message => {
+            const createDiv = document.createElement("div");
+
+            if(message.admin_id === null){
+                createDiv.className = "admin_message_client";
+
+                createDiv.innerHTML = `<span>${connection.user.email}</span>`;
+                createDiv.innerHTML += `<span>${message.text}</span>`;
+                createDiv.innerHTML += `<span class="admin-date">${dayjs(message.created_at).format("DD/MM/YYYY HH:mm:ss")}</span>`
+            }else {
+                createDiv.className = "admin_message_admin";
+
+                createDiv.innerHTML = `<span>Atendente: - ${message.text} </span>`;
+                createDiv.innerHTML += `<span class="admin-date">${dayjs(message.created_at)
+                    .format("DD/MM/YYYY HH:mm:ss")}`
+            }
+
+            divMessages.appendChild(createDiv);
+        });
+    });
+}
--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -1,11 +1,21 @@
 // # Own files
 import { io } from "../http";
 import { ConnectionsService } from "../services/ConnectionsService";
+import { MessagesService } from "../services/MessagesService";
 
 io.on("connect", async (socket) => {
     const connectionsService = new ConnectionsService();
+    const messagesService = new MessagesService();
 
     const allConnectionsWithoutAdmin = await connectionsService.findAllWithoutAdin();
 
     io.emit("admin_list_all_users", allConnectionsWithoutAdmin);
-})
+
+    socket.on("admin_list_messages_by_user", async (params, callback) => {
+        const { user_id } = params;
+
+        const allMessages = await messagesService.listByuser(user_id);
+
+        callback(allMessages);
+    });
+});
